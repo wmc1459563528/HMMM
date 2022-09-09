2fc3602798d08e2c36c6f8e7b72f7d4d8f1db67f
A Lucky Boy <1459563528@qq.com>
2022-09-09 22:36:56 +0800

    面试技巧完成

--- src/module-hmmm/components/articles-add.vue
+++ src/module-hmmm/components/articles-add.vue
@@ -1,66 +1,121 @@
 <template>
-  <div class="container">
-    <!-- 编辑/新增对话框 -->
-    <el-dialog
-      title="新增文章"
-      :visible="isShow"
-      @close="handleClose"
-    >
-      <el-form
-        ref="form"
-        :model="data"
-        label-width="80px"
-      >
-        <el-form-item label="文章标题">
-          <el-input v-model="data.title"></el-input>
+  <div class="articles-add">
+    <el-dialog :title="`${data.id?'修改':'新增'}文章`" :visible.sync="show" width="800px">
+      <el-form ref="form" size="small" label-width="100px" :model="formModel" :rules="formRules">
+        <el-form-item label="文章标题：" prop="title">
+          <el-input v-model="formModel.title" placeholder="请输入文章标签"></el-input>
         </el-form-item>
-        <el-form-item label="文章内容">
-          <!-- 富文本编辑器 -->
-          <VueUeditorWrap />
+        <el-form-item label="文章内容：" prop="articleBody">
+          <quill-editor v-model="formModel.articleBody" :options="editorOptions"></quill-editor>
         </el-form-item>
-        <el-form-item label="视频地址">
-            <el-input v-model="data.videoURL"></el-input>
+        <el-form-item label="视频地址：" prop="videoURL">
+          <el-input v-model="formModel.videoURL" placeholder="请输入视频地址"></el-input>
         </el-form-item>
       </el-form>
-      <span slot="footer" class="dialog-footer">
-        <el-button @click="handleClose">取消</el-button>
-        <el-button type="primary" @click="handleSubmit">确定</el-button>
-      </span>
+      <div slot="footer">
+        <el-button @click="show=false">取消</el-button>
+        <el-button @click="confirm()" type="primary">确认</el-button>
+      </div>
     </el-dialog>
   </div>
 </template>
 
 <script>
-// 富文本编辑器
-import VueUeditorWrap from 'vue-ueditor-wrap' // ES6 Module
+import { add, update } from '@/api/hmmm/articles'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+import { quillEditor } from 'vue-quill-editor'
 export default {
   name: 'ArticlesAdd',
+  components: { quillEditor },
   props: {
-    isShow: {
-      type: Boolean,
-      default: false
-    },
     data: {
-      type: Object,
-      default: () => {}
+      default: () => {},
+      type: Object
     }
-
   },
-  components: {
-    VueUeditorWrap
+  data () {
+    return {
+      show: false,
+      formModel: {
+        id: null,
+        title: null,
+        articleBody: null,
+        videoURL: null
+      },
+      formRules: {
+        // 校验表单必填项
+        title: [
+          { required: true, message: '请输入文章标题', tirgger: ['blur', 'change'] }
+        ],
+        articleBody: [
+          { required: true, message: '请输入文章内容', tirgger: ['blur', 'change'] }
+        ]
+      },
+      editorOptions: {
+        placeholder: '请输入正文',
+        modules: {
+          toolbar: [
+            ['bold', 'italic', 'underline', 'strike'], // 加粗 斜体 下划线 删除线
+            ['blockquote', 'code-block'], // 引用 代码块
+            [{ list: 'ordered' }, { list: 'bullet' }], // 有序、无序列表
+
+            [{ header: 1 }, { header: 2 }], // 1、2 级标题
+            ['image', 'video', 'link'], // 链接、图片、视频、链接
+            [{ indent: '-1' }, { indent: '+1' }], // 缩进
+            ['clean'] // 清除文本格式
+          ]
+
+        }
+      }
+    }
   },
   methods: {
-    handleClose () {
-      this.$emit('update:isShow', false)
-      // console.log(this.data)
+    open () {
+      this.show = true
+      if (this.data.id) {
+        const { id, title, articleBody, videoURL } = this.data
+        this.formModel = { id, title, articleBody, videoURL }
+      } else {
+        this.formModel = {
+          id: null,
+          title: null,
+          articleBody: null,
+          videoURL: null
+        }
+      }
+      // 清除校验效果
+      this.$nextTick(() => {
+        this.$refs.form.clearValidate()
+      })
     },
-    handleSubmit () {
-
+    async confirm () {
+      this.$refs.form.validate(async valid => {
+        if (valid) {
+          // 提交
+          if (!this.data.id) {
+            // 新增
+            await add(this.formModel)
+          } else {
+            // 修改
+            this.formModel.id = this.data.id
+            await update(this.formModel)
+          }
+          this.$message.success('操作成功')
+          this.show = false
+          this.$emit('updateList')
+        }
+      })
     }
   }
 }
 </script>
-
-<style scoped lang="less">
-
+<style lang="scss" scoped>
+.articles-add ::v-deep .ql-editor{
+  height: 200px;
+}
+.articles-add ::v-deep .ql-toolbar.ql-snow{
+  padding: 0 8px;
+}
 </style>
